96f66469c64ed87dd86720cce83545dca3b567b6
Acted <t-913.saki@outlook.jp>
2025-03-25 20:52:12 +0900

    Initial commit

---
+++ src/main/java/si/f5/actedmc/logDiscordMC/LogDiscordMC.java
@@ -0,0 +1,17 @@
+package si.f5.actedmc.logDiscordMC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LogDiscordMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
